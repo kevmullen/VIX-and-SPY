1fedf009baa0da9392fc7521f96300f1835472c6
Kevin Mullen <41759167+kevmullen@users.noreply.github.com>
2019-08-19 14:17:26 -0400

    Add files via upload

---
+++ vix spy thing.R
@@ -0,0 +1,33 @@
+vix <- c(1:6449)
+rr <- c(1:6449)
+df <- data.frame(vix,rr)
+k <- 1
+for (i in 0:50){
+  for (j in 1:6439){
+   if (vi_spy_and_return$v[j] <= i + .5 && vi_spy_and_return$v[j] >= i - .5) {
+     df$vix[k] <- i
+     df$rr[k] <- vi_spy_and_return$r[j]
+     k <- k+1
+   }
+  }
+}
+ggplot(df,aes(rr)) + geom_density(kernel = 'biweight') + facet_wrap(~ vix,scales = "free")
+
+ggplot(df, aes(rr)) + 
+  geom_histogram(aes(y=..density..),bins = 15, colour="black", fill="white")+
+  geom_density(kernel = 'biweight',fill="#FF6666",alpha=.2) + 
+  facet_wrap(~ vix, scales = "free") + 
+  geom_vline(aes(xintercept = mean(rr)),col='red',size=.5)
+
+m<- c()
+n<- c()
+for (i in 9:30) {
+  m <- c(m,mean(df[(df$vix == i),'rr']))
+  n <- c(n,i)
+}
+df3 <- data.frame(n,m)
+  
+
+
+
+ggplot(vi_spy_and_return,aes(v,r)) + geom_boxplot(aes(group = cut_width(v, 1))) + xlim(9,20) + ylim(-3,3)
